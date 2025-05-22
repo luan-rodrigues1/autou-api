fd23141ea697037d2835ede226d7b96715031154
luan-rodrigues1 <rodrigues.luan20@gmail.com>
2025-05-21 23:47:10 -0300

    fix: add CORS

--- app/main.py
+++ app/main.py
@@ -3,9 +3,19 @@ Módulo principal da aplicação.
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import base
 
 app = FastAPI()
+
+# Configuração do CORS para aceitar qualquer origem
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 # Registrar as rotas
 app.include_router(base.router)
